51c5af1b9b5268780af7256ad746a536bf37c2cd
rudraprasaaad <goudarudra23@gmail.com>
2025-06-15 02:07:45 +0530

    used useDispatch

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import store from "./store.js";
+import { Provider } from "react-redux";
 
-store.dispatch({ type: "account/deposit", payload: 500 });
-console.log(store.getState());
-
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
