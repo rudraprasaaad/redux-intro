9bb1c74c73b99a0c17c48bc70e34904c10c46b85
rudraprasaaad <goudarudra23@gmail.com>
2025-06-15 00:54:22 +0530

    added redux

---
+++ src/store.js
@@ -0,0 +1,127 @@
+import { createStore } from "redux";
+
+const initialStateAccount = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+function accountReducer(state = initialStateAccount, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+      };
+
+    case "account/withdraw":
+      return {
+        ...state,
+        balance: state.balance - action.payload,
+      };
+
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+
+    default:
+      return state;
+  }
+}
+
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+
+    case "customer/updateName":
+      return {
+        ...state,
+        fullName: action.payload,
+      };
+  }
+}
+
+const accountStore = createStore(accountReducer);
+const customerStore = createStore(customerReducer);
+
+function deposit(amount) {
+  return {
+    type: "account/deposit",
+    payload: amount,
+  };
+}
+
+function withdraw(amount) {
+  return {
+    type: "account/withdraw",
+    payload: amount,
+  };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+
+function payLoan() {
+  return {
+    type: "account/payLoan",
+  };
+}
+
+accountStore.dispatch(deposit(500));
+// console.log(accountStore.getState());
+accountStore.dispatch(withdraw(200));
+// console.log(accountStore.getState());
+
+accountStore.dispatch(requestLoan(1000, "To buy a car"));
+// console.log(accountStore.getState());
+accountStore.dispatch(payLoan());
+// console.log(accountStore.getState());
+
+customerStore.dispatch(createCustomer("Rudraprasad", "Indian"));
+console.log(customerStore.getState());
+
+customerStore.dispatch(updateName("Rudra"));
+console.log(customerStore.getState());
+
+function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+function updateName(fullName) {
+  return {
+    type: "customer/updateName",
+    payload: fullName,
+  };
+}
